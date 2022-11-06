4516bd571cfc18484246ab2e0387832d80751311
Mercy <mercycheruiyot2013@gmail.com>
2022-11-06 21:05:22 +0300

    Completed Assignement

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
+    create_table :characters do |t|
+      t.integer :actor_id
+      t.integer :show_id
+      t.string :name
+    end
     # Your code here
   end
 end
